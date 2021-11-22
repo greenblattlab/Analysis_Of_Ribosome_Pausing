24cb357cc5f0e4d514aa3ebec8e48619cefb161e
Keegan <flanagan.keegan16@gmail.com>
2021-11-22 11:54:22 -0800

    Fixed profile arrays to use codon resolution

--- tools/keegan_analysis_tools.py
+++ tools/keegan_analysis_tools.py
@@ -583,3 +583,180 @@ def big_dif_mmus(diff_dist, transcripts, data_mutant, data_control, figsize = (1
     fig.tight_layout()
     
     return ax
+
+def low_density(lamb,a,I = 10):
+    '''
+    A function that calculates the particle density along a transcript from a set of elongation rates
+    inferred from ribosome profiling. This function assumes that elongation is in a low density regime
+    (e.g. initiation limiting)
+    '''
+    Jl = (a*(lamb[0]-a))/(lamb[0] + (I-1)*a)
+    pl = 1/(2*I) + (Jl*(I-1))/(2*I*lamb) - np.sqrt((1/(2*I) + (Jl*(I-1))/(2*I*lamb))**2 - Jl/(I*lamb))
+    return(pl) 
+
+def high_density(lamb,B,I = 10):
+    '''
+    A function that calculates the particle density along a transcript from a set of elongation rates
+    inferred from ribosome profiling. This function assumes that elongation is in a high density regime
+    (e.g. termination limiting)
+    '''
+    JR = (B*(lamb[-1]-B))/(lamb[-1] + (I-1)*B)
+    pR = 1/(2*I) + (JR*(I-1))/(2*I*lamb) + np.sqrt((1/(2*I) + (JR*(I-1))/(2*I*lamb))**2 - JR/(I*lamb))
+    return(pR) 
+
+def maximum_current(lamb,a,B,I = 10):
+    '''
+    A function that calculates the particle density along a transcript from a set of elongation rates
+    inferred from ribosome profiling. This function assumes that elongation is in a maximum current regime
+    (e.g. elongation limiting)
+    '''
+    Jmax = min(lamb)/((1+np.sqrt(I))**2)
+    flip = np.where(lamb == np.amin(lamb))[0][0]
+    pR = 1/(2*I) + (Jmax*(I-1))/(2*I*lamb[0:flip]) + np.sqrt((1/(2*I) + (Jmax*(I-1))/(
+        2*I*lamb[0:flip]))**2 - Jmax/(I*lamb[0:flip]))
+    pl = 1/(2*I) + (Jmax*(I-1))/(2*I*lamb[flip:]) - np.sqrt((1/(2*I) + (Jmax*(I-1))/(
+        2*I*lamb[flip:]))**2 - Jmax/(I*lamb[flip:]))
+    p = np.concatenate((pR,pl))
+    return(p) 
+
+def make_mc(arr_c, position, a, B, I = 10):
+    '''
+    This function purposefully induces elongation limitation at a certain point 
+    '''
+    lamb_c = copy.deepcopy(arr_c)
+    Jmax = min(lamb_c)/((1+np.sqrt(I))**2)
+    crit_a = ((lamb_c[0] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[0]*Jmax)/((lamb_c[0] - (I - 1)*Jmax)**2)))
+    crit_B = ((lamb_c[-1] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[-1]*Jmax)/((lamb_c[-1] - (I - 1)*Jmax)**2)))
+    mut_min = position
+    while True:
+        lamb_c[mut_min] = lamb_c[mut_min]*0.9 # It keeps doing this every run through. 
+        Jmax = min(lamb_c)/((1+np.sqrt(I))**2)
+        crit_a = ((lamb_c[0] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[0]*Jmax)/((lamb_c[0] - (I - 1)*Jmax)**2)))
+        crit_B = ((lamb_c[-1] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[-1]*Jmax)/((lamb_c[-1] - (I - 1)*Jmax)**2)))
+        if crit_a < a and crit_B < B:
+            break
+    p = maximum_current(lamb_c,a=a,B=B,I = 10)
+    return p
+
+####!!!!#### If I am going to keep this function I better do something about the possibility of a high density regime.
+def make_ld(lamb, a, B, I = 10):
+    '''
+    This function attempts to 
+    '''
+    lamb_c = copy.deepcopy(lamb)# Create a copy of lamb to work on. 
+    Jmax = min(lamb_c)/((1+np.sqrt(I))**2)
+    crit_a = ((lamb_c[0] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[0]*Jmax)/((lamb_c[0] - (I - 1)*Jmax)**2)))
+    crit_B = ((lamb_c[-1] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[-1]*Jmax)/((lamb_c[-1] - (I - 1)*Jmax)**2)))
+    if a < crit_a and B > crit_B:
+        p = low_density(lamb_c, a, I)
+    else:
+        while True:
+            min_l = np.where(lamb_c == np.amin(lamb_c))[0][0]
+            lamb_c[min_l] = lamb_c[min_l]*1.1 # It keeps doing this every run through. 
+            Jmax = min(lamb_c)/((1+np.sqrt(I))**2)
+            crit_a = ((lamb_c[0] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[0]*Jmax)/((lamb_c[0] - (I - 1)*Jmax)**2)))
+            crit_B = ((lamb_c[-1] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[-1]*Jmax)/((lamb_c[-1] - (I - 1)*Jmax)**2)))
+            if a < crit_a and B > crit_B:
+                break
+        p = low_density(lamb_c, a, I) 
+    return p
+
+def get_density(lamb, a, B, I = 10, intermediates = False):
+    '''A function that determines the correct elongation  regime/phase and calculates the correct density 
+       accordingly
+       
+       arg lamb: A set of elongation rates for each codon in a transcript
+       type: list of floats
+       
+       arg a: An initiation rate (the rate at which ribosomes are added to the transcript)
+       type: float
+       
+       arg B: A termination rate (the rate a which ribosomes leave the transcript)
+       type: float
+       
+       arg I: The particle size to be used in the TASEP model (should equal the size of the ribosome in codons)
+       type: int
+       
+       arg intermediates: If set to true than the function will output all intermediate values used to calculate densities
+       type: bool 
+    
+    '''
+    lamb_c = copy.deepcopy(lamb)# Create a copy of lamb to work on. 
+    Jmax = min(lamb_c)/((1+np.sqrt(I))**2)
+    crit_a = ((lamb_c[0] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[0]*Jmax)/((lamb_c[0] - (I - 1)*Jmax)**2)))
+    crit_B = ((lamb_c[-1] - (I-1) * Jmax) / 2)*(1 - np.sqrt(1 - (4*lamb_c[-1]*Jmax)/((lamb_c[-1] - (I - 1)*Jmax)**2)))
+    if a < crit_a and B > crit_B:
+        p = low_density(lamb_c, a, I)
+        density = "LD"
+    elif a > crit_a and B < crit_B:
+        p = high_density(lamb_c, B, I)
+        density = "HD"
+    elif a < crit_a and B < crit_B:
+        Jl = (a*(lamb[0]-a))/(lamb[0] + (I-1)*a)
+        JR = (B*(lamb[-1]-B))/(lamb[-1] + (I-1)*B)
+        sign = Jl - JR 
+        if sign > 0:
+            p = low_density(lamb_c, a, I)
+            density = "LD"
+        elif sign < 0:
+            p = high_density(lamb_c, B, I)
+            density = "HD"
+    elif a > crit_a and B > crit_B:
+        p = maximum_current(lamb_c, a, B, I)
+        density = "MC"
+    if intermediates == False:
+        return p, density
+    elif intermediates == True:
+        return p, a, B, crit_a, crit_B, min(lamb_c), lamb_c[0]
+
+# Create a function to automate the simulation process
+def simulate_profile(mean_lambda, sd, length, a, B = 2, read_density = 1):
+    '''
+    A function that simulates ribosome profiling data from a mutant and a control for a single gene. 
+    
+    mean_lambda: The mean elongation rate used for the simulated gene
+    type: float
+    
+    sd: The standard deviation of the elongation rates for your simulated gene
+    type: float
+    
+    length: The length of your simulated gene
+    type: int
+    
+    a: The initiation rate of your simulated gene
+    type: float
+    
+    B: The termination rate of your simulated gene
+    type: float
+    
+    read_density: how many reads should be extracted from your simulated gene per codon. 
+    type: int
+    
+    '''
+    mean_sqr = np.sqrt(mean_lambda)
+    sample_size = int(read_density * length)
+    lamb_c = np.random.gamma(mean_sqr, sd, length)+0.3
+    
+    # Create a set of random pause sights in your mutant. 
+    pause_sites = np.random.randint(10, length/2, 4)
+    lamb_m = copy.deepcopy(lamb_c)
+    for ps in pause_sites:
+        lamb_m[ps] = lamb_m[ps]* np.random.uniform(0.1, 0.8, 1)[0]
+    
+    # simulate the reads for the control
+    p_c = get_density(lamb_c, a, B)
+    prob_c = p_c[0]/sum(p_c[0])
+    reads_c = np.zeros(length)
+    for i in range(sample_size):
+        x = numpy.random.choice(np.arange(0, len(prob_c)), p = prob_c)
+        reads_c[x] = reads_c[x]+1
+
+    # simulate the reads for the mutant assuming LD
+    mu_a = a* np.random.uniform(0.25, 2, 1)[0]
+    p_m = get_density(lamb_m, mu_a, B)
+    prob_m = p_m[0]/sum(p_m[0])
+    reads_m = np.zeros(length)
+    for i in range(sample_size):
+        x = numpy.random.choice(np.arange(0, len(prob_m)), p = prob_m)
+        reads_m[x] = reads_m[x]+1
+    return reads_c, reads_m
