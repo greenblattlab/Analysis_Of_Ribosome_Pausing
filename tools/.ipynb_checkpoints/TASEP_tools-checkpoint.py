11867d24da480a4a8fb4999487fa552771d84e79
Keegan <flanagan.keegan16@gmail.com>
2021-12-10 15:23:26 -0800

    Improved proportional analysis graphs

--- tools/.ipynb_checkpoints/TASEP_tools-checkpoint.py
+++ tools/.ipynb_checkpoints/TASEP_tools-checkpoint.py
@@ -299,7 +299,7 @@ def simulate_profile(mean_lambda = 4, sd = 3, min_lambda = 0.8, length = 400, a 
         
     # Create a set of random pause sights in your mutant. 
     pause_sites = []
-    for i in range(round(length/2)):
+    for i in range(round(length)):
         if random.random() < pause_density:
             pause_sites.append(i)
     if rng_pause == True:
