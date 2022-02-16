3541ade8397a565f3705c8430cca5df1bd35d3f7
Keegan <flanagan.keegan16@gmail.com>
2022-02-16 13:21:30 -0800

    swtiched to seleno data

---
+++ STAR_Protocol/mmus_genome_alignment/step_12_index_genome.sh
@@ -0,0 +1,1 @@
+STAR --runThreadN 8 --limitGenomeGenerateRAM 20000000000 --runMode genomeGenerate --genomeDir . --genomeFastaFiles mm10.fa --sjdbGTFfile mm10.refGene.gtf --sjdbOverhang 100 --genomeSAindexNbases 13
